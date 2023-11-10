7041f1d7f1b77ef66233620258b2de1aba977408
Nicole Åhgren <ahgren.nicole@elev.ga.ntig.se>
2023-11-10 11:55:25 +0100

    tamagochi

--- VinterProjekt/Program.cs
+++ VinterProjekt/Program.cs
@@ -1,3 +1,53 @@
 ﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Tamagochi tama = new();
+string input;
+
+Console.WriteLine("Välj ett namn till din tamagochi");
+tama.name = Console.ReadLine();
+
+while (tama.GetAlive() == true)
+{
+    tama.PrintStats();
+
+    Console.WriteLine($"Vad göra med {tama.name}");
+    Console.WriteLine("1. Mata den");
+    Console.WriteLine("2. Hälsa på den");
+    Console.WriteLine("3. Lära den ett nytt ord");
+    Console.WriteLine("4. Ingenting");
+    input = Console.ReadLine();
+    if (input == "3")
+    {
+        Console.WriteLine($"Vad vill lära din {tama.name} för ord?");
+        tama.TeachWord(Console.ReadLine());
+        Console.WriteLine($"{tama.name} kan ett nytt ord nu!");
+        tama.Tick();
+        Console.ReadLine();
+        
+    }
+    else if (input == "2")
+    {
+        tama.Hello();
+        tama.Tick();
+        Console.ReadLine();
+    }
+    else if (input == "1")
+    {
+        Console.WriteLine($"Du gav {tama.name} en bit pizza");
+        tama.Feed();
+        tama.Tick();
+        Console.ReadLine();
+    }
+    else if (input == "4")
+    {
+        tama.Tick();
+    }
+    else
+    {
+        Console.WriteLine("Nej du kan inte skriva så.");
+        Console.ReadLine();
+    }
+    Console.Clear();
+}
+
+Console.WriteLine($"{tama.name} är nu död...");
+Console.ReadLine();
---
+++ VinterProjekt/Tamagochi.cs
@@ -0,0 +1,58 @@
+﻿public class Tamagochi
+{
+    int hunger = 0;
+    int boredom = 0;
+    List<string> words = new() {"cheeeese"};
+    bool isAlive = true;
+
+    Random generator  = new Random();
+
+    public string name;
+
+    public void PrintStats()
+    {
+        Console.WriteLine($"Name: {name} || Hunger: {hunger} || Boredom: {boredom} || Vocabulary: {words.Count} words");
+    }
+
+    public void Feed()
+    {
+        hunger -= 10;
+        Console.WriteLine($"Hunger = {hunger}" );
+    }
+    public bool GetAlive()
+        {
+            return isAlive;
+        }
+
+    private void ReduceBoredom()
+    {
+        boredom -= 10;
+    }
+        public void Tick()
+    {
+        boredom += generator.Next(5);
+        hunger += generator.Next(7);
+        if (boredom >= 100 || hunger >= 100)
+        {
+            isAlive = false;
+        }
+    }
+    public void TeachWord(string word)
+    {
+        words.Add (word);
+       ReduceBoredom();
+        return;
+    }
+    public void Hello()
+    {
+        int wordNr = generator.Next(words.Count);
+        Console.WriteLine($" {name} says: {words[wordNr]}");
+        ReduceBoredom();
+    }
+}
+
+
+
+
+
+
